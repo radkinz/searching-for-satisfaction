ff5b95319724783309e6fab66435efc4b608b509
Ruby Adkins <radkins22@students.stab.org>
2021-12-08 13:43:37 -0500

    use multi-word queries

--- search.js
+++ search.js
@@ -6,56 +6,88 @@ const fs = require('fs')
 let query = process.argv
 
 //grab word index
-let wordIndex = JSON.parse(fs.readFileSync('wordIndex.txt'));
+let wordIndex = JSON.parse(fs.readFileSync('wordIndex.txt'))
 
 //grab title index
-let titleIndex = JSON.parse(fs.readFileSync('indexforIDs.txt'));
+let titleIndex = JSON.parse(fs.readFileSync('indexforIDs.txt'))
 
 //grab freq index
-let freqIndex = JSON.parse(fs.readFileSync('freqIndex.txt'));
+let freqIndex = JSON.parse(fs.readFileSync('freqIndex.txt'))
 
-//stem query of user input
-let userinput = stemmer(query[2])
+//loop through different queries
+let finalrankings = {}
+for (let i = 2; i < query.length; i++) {
+  let ranking = getFreqRanking(query[i])
 
-console.log(getFreqRanking(userinput))
+  //loop through ranking and add to final rankings
+  for (const key in ranking) {
+    if (finalrankings[key]) {
+      finalrankings[key] += sq(ranking[key])
+    } else {
+      finalrankings[key] = ranking[key]
+    }
+  }
+}
+
+//sort final ranking list
+let sortedrankings = Object.keys(finalrankings).sort(function(a,b){return finalrankings[b]-finalrankings[a]})
+console.log(sortedrankings)
 
 //function returns a ranked list by freq of one word queries
 function getFreqRanking (word) {
+  //stem words
+  word = stemmer(word)
   //grab id list with userInput from word index
-  let ids = wordIndex[userinput];
+  let ids = wordIndex[word]
+
+  //check if word no exist
+  if (ids == undefined) {
+    return {};
+  }
 
   //create double list to correspond every id with word freq of user input to rank
-  let ranking = [];
+  let ranking = []
 
   //loop through ids and get freq
   for (let i = 0; i < ids.length; i++) {
     ///* BEGINING RANKING ALGO *///
     //grab freq of word in each id
-    let freqofWord = freqIndex[ids[i]][userinput];
+    let freqofWord = freqIndex[ids[i]][word]
 
     //grab total freq of all words to calc ratio
-    let totalFreq = 0;
+    let totalFreq = 0
     for (const key in freqIndex[ids[i]]) {
-      totalFreq += parseInt(freqIndex[ids[i]][key]);
+      totalFreq += parseInt(freqIndex[ids[i]][key])
     }
 
     //grab ratio of one word over total
-    let ratio = (freqofWord / totalFreq) * 100;
+    let ratio = (freqofWord / totalFreq) * 100
 
     //covert id to title
-    let title = titleIndex[ids[i]];
+    let title = titleIndex[ids[i]]
 
     //add to object
-    ranking.push([title, ratio]);
+    ranking.push([title, ratio])
   }
 
   //use quick sort and sort ranking by ratio
-  quickSort(ranking, 0, ranking.length - 1);
+  quickSort(ranking, 0, ranking.length - 1)
 
   //reverse ranking from low to high => high to low
-  ranking.reverse();
+  ranking.reverse()
 
-  return ranking
+  //if list is longer than 20...cut it
+  if (ranking.length > 20) {
+      ranking = ranking.splice(0, 20);
+  }
+
+  //covert odd list format to nice obj format
+  let rankingObject = {}
+  for (let i = 0; i < ranking.length; i++) {
+    rankingObject[ranking[i][0]] = ranking[i][1]
+  }
+
+  return rankingObject
 }
 
 function quickSort (arr, start, end) {
